30f401fa3f8468053894493c1a2dce74917d50e4
Arnab Dutta <arnabduttasocial@gmail.com>
2021-08-06 11:43:14 +0530

    Updates

---
+++ precache-manifest.2d6d42e0c4f939dc039349d578e83277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c82d8a30aee4ae7a382988e4a891da",
+    "url": "/sketch-card/index.html"
+  },
+  {
+    "revision": "aaa89730a5ad6e4cb7b7",
+    "url": "/sketch-card/static/css/main.2c6adc08.chunk.css"
+  },
+  {
+    "revision": "fb975071de66c67c97c4",
+    "url": "/sketch-card/static/js/2.85495163.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sketch-card/static/js/2.85495163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa89730a5ad6e4cb7b7",
+    "url": "/sketch-card/static/js/main.9f69865d.chunk.js"
+  },
+  {
+    "revision": "1a49a9bff690863795ff",
+    "url": "/sketch-card/static/js/runtime-main.921af9c0.js"
+  }
+]);
